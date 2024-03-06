fbd89f4340b6bc63762697ce877a4516d86ecf2d
kvatt1 <vattikondakrishnaveni85@gmail.com>
2024-03-05 20:42:24 -0500

    Midterm

---
+++ script.js
@@ -0,0 +1,8 @@
+function scrollToSection(Id){
+    var elementid=document.getElementById(Id);
+    elementid.scrollIntoView({behavior:'smooth'});
+}
+
+function openLink(url){
+    window.location.href=url;
+}
